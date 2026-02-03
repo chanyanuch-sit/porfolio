f83845302ec6f911196faf3506bfbd429faa522b
nan <chanyanuch.sit@gmail.com>
2026-02-03 10:16:44 +0700

    remove animate

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,17 @@
 // animation item game, section
-const items = document.querySelectorAll('.section--about, .section--experience, .section--skills, .section--contact');
+// const items = document.querySelectorAll('.section--about, .section--experience, .section--skills, .section--contact');
 
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach(entry => {
-		if (entry.isIntersecting) {
-		entry.target.classList.add('animate');
-		}
-	});
-	}, {
-	threshold: 0.5
-});
+// const observer = new IntersectionObserver((entries) => {
+// 	entries.forEach(entry => {
+// 		if (entry.isIntersecting) {
+// 		entry.target.classList.add('animate');
+// 		}
+// 	});
+// 	}, {
+// 	threshold: 0.5
+// });
 
-items.forEach((item, index) => {
-	item.style.transitionDelay = `${index * 0.1}s`;
-	observer.observe(item);
-});
+// items.forEach((item, index) => {
+// 	item.style.transitionDelay = `${index * 0.1}s`;
+// 	observer.observe(item);
+// });
