9f8feb00422343a54b94b090665afb87a24a59a7
nan <chanyanuch.sit@gmail.com>
2026-02-27 01:41:17 +0700

    update deatils

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,20 @@
 // animation item game, section
-// const items = document.querySelectorAll('.section--about, .section--experience, .section--skills, .section--contact');
+const items = document.querySelectorAll('.section--about, .section--experience, .section--skills, .section--projects, .section--contact, .section--content img');
 
-// const observer = new IntersectionObserver((entries) => {
-// 	entries.forEach(entry => {
-// 		if (entry.isIntersecting) {
-// 		entry.target.classList.add('animate');
-// 		}
-// 	});
-// 	}, {
-// 	threshold: 0.5
-// });
+const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('animate');
+			observer.unobserve(entry.target); // Stop observing once animated
+		}
+	});
+}, {
+	threshold: 0.01,
+	rootMargin: "0px 0px -50px 0px"
+});
 
-// items.forEach((item, index) => {
-// 	item.style.transitionDelay = `${index * 0.1}s`;
-// 	observer.observe(item);
-// });
+items.forEach((item, index) => {
+	// Optional: uncomment if you want staggered delays for each section
+	// item.style.transitionDelay = `${index * 0.1}s`;
+	observer.observe(item);
+});
